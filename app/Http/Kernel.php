10d2ef2bae12b0e9627ced803f00320e16117913
Solomon Ochepa <solomonochepa@gmail.com>
2023-03-17 21:37:08 +0100

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -68,6 +68,5 @@ class Kernel extends HttpKernel
         'permission'         => \Spatie\Permission\Middlewares\PermissionMiddleware::class,
         'role_or_permission' => \Spatie\Permission\Middlewares\RoleOrPermissionMiddleware::class,
         'frontend'          => \App\Http\Middleware\FrontEnd::class,
-
     ];
 }
