193c4bfb5b672f45cf19720f62bf46d431d4fb65
Solomon Ochepa <110875083+solomon-ochepa@users.noreply.github.com>
2023-04-08 14:26:36 +0100

    Update index.blade.php

--- Modules/Apartment/resources/views/livewire/admin/room/index.blade.php
+++ Modules/Apartment/resources/views/livewire/admin/room/index.blade.php
@@ -6,13 +6,9 @@
         <table class="table table-hover">
             <thead>
                 <tr>
-                    <th class="">
-                        <input disabled type="checkbox" name="select_all" id="select_all" wire:model.defer="select_all" />
-                    </th>
-                    <th>{{ __('label.name') }}</th>
-                    <th>{{ __('resident.resident') }}</th>
-                    <th>{{ __('visitor.visitors') }}</th>
-                    <th>{{ __('label.active') }}</th>
+                    <th>{{ __('Name') }}</th>
+                    <th>{{ __('Tenants') }}</th>
+                    <th>{{ __('Visitors') }}</th>
                     <th class="text-right"></th>
                 </tr>
             </thead>
@@ -21,13 +17,15 @@
                 @foreach ($rooms ?? [] as $key => $room)
                     <tr>
                         <td>
-                            <input disabled type="checkbox" name="selected" id="selected"
-                                wire:model.defer="selected.{{ $key }}.{{ $room->id }}" />
-                        </td>
-                        <td>
                             <a class="text-dark"
                                 href="{{ route('admin.apartment.room.show', ['apartment' => $room->roomable->slug, 'room' => $room->slug]) }}">
                                 {{ $room->name }}
+
+                                @if ($room->active)
+                                    <i class="fas fa-check-circle text-success"></i>
+                                @else
+                                    <i class="fas fa-times-circle text-danger"></i>
+                                @endif
                             </a>
                         </td>
                         <td>
@@ -36,27 +34,21 @@
                         <td>
                             {{-- {{ $room->visitors->count() }} --}}
                         </td>
-                        <td>
-                            @if ($room->active)
-                                <i class="fas fa-check text-success"></i>
-                                Active
-                            @else
-                                <i class="fas fa-times text-danger"></i>
-                                Disabled
-                            @endif
-                        </td>
-                        <td class="text-right">
-                            <a href="#" class="btn btn-sm text-decoration-none">
+
+                        {{-- Actions --}}
+                        <td class="text-end">
+                            <a href="{{ route('admin.apartment.room.show', ['apartment' => $room->roomable->slug, 'room' => $room->slug]) }}"
+                                class="btn btn-sm text-decoration-none">
                                 <i class="fas fa-eye"></i>
                             </a>
 
-                            <a href="#" class="btn btn-sm text-decoration-none">
+                            {{-- <a href="#" class="btn btn-sm text-decoration-none">
                                 <i class="fas fa-edit"></i>
-                            </a>
+                            </a> --}}
 
-                            <a href="#" class="btn btn-sm text-decoration-none">
+                            {{-- <a href="#" class="btn btn-sm text-decoration-none">
                                 <i class="fas fa-trash text-danger"></i>
-                            </a>
+                            </a> --}}
                         </td>
                     </tr>
                 @endforeach
