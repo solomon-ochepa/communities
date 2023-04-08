e2371244621ed5789cc7b9302e210fdafbab04ab
Solomon Ochepa <110875083+solomon-ochepa@users.noreply.github.com>
2023-04-08 12:35:38 +0100

    Apartment Rooms (index)

--- Modules/Apartment/app/Http/Livewire/Admin/Room/Index.php
+++ Modules/Apartment/app/Http/Livewire/Admin/Room/Index.php
@@ -3,11 +3,37 @@
 namespace Modules\Apartment\app\Http\Livewire\Admin\Room;
 
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Index extends Component
 {
+    use WithPagination;
+
+    public $data = [];
+
+    public $apartment;
+
+    public $search = "";
+    public $limit = 25;
+    public $page = 1;
+    protected $queryString = [
+        'search' => ['except' => ''],
+        'page' => ['except' => 1],
+    ];
+
     public function render()
     {
-        return view('apartment::livewire.admin.room.index');
+        if ($this->search) {
+            $this->data['rooms'] = $this->apartment->rooms()
+                ->where('name', 'like', '%' . $this->search . '%')
+                ->orderBy('name')
+                ->paginate($this->limit);
+        } else {
+            $this->data['rooms'] = $this->apartment->rooms()
+                ->orderBy('name')
+                ->paginate($this->limit);
+        }
+
+        return view('apartment::livewire.admin.room.index', $this->data);
     }
 }
--- Modules/Apartment/resources/views/livewire/admin/room/index.blade.php
+++ Modules/Apartment/resources/views/livewire/admin/room/index.blade.php
@@ -1,3 +1,72 @@
-<div>
-    <h3>The <code>Index</code> livewire component is loaded from the <code>Apartment</code> module.</h3>
+<div class="card-body">
+    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+    <x-page-search />
+
+    <div class="table-responsive">
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th class="">
+                        <input disabled type="checkbox" name="select_all" id="select_all" wire:model.defer="select_all" />
+                    </th>
+                    <th>{{ __('label.name') }}</th>
+                    <th>{{ __('resident.resident') }}</th>
+                    <th>{{ __('visitor.visitors') }}</th>
+                    <th>{{ __('label.active') }}</th>
+                    <th class="text-right"></th>
+                </tr>
+            </thead>
+
+            <tbody>
+                @foreach ($rooms ?? [] as $key => $room)
+                    <tr>
+                        <td>
+                            <input disabled type="checkbox" name="selected" id="selected"
+                                wire:model.defer="selected.{{ $key }}.{{ $room->id }}" />
+                        </td>
+                        <td>
+                            <a class="text-dark"
+                                href="{{ route('admin.apartment.room.show', ['apartment' => $room->roomable->slug, 'room' => $room->slug]) }}">
+                                {{ $room->name }}
+                            </a>
+                        </td>
+                        <td>
+                            {{ $room->tenants->count() }}
+                        </td>
+                        <td>
+                            {{-- {{ $room->visitors->count() }} --}}
+                        </td>
+                        <td>
+                            @if ($room->active)
+                                <i class="fas fa-check text-success"></i>
+                                Active
+                            @else
+                                <i class="fas fa-times text-danger"></i>
+                                Disabled
+                            @endif
+                        </td>
+                        <td class="text-right">
+                            <a href="#" class="btn btn-sm text-decoration-none">
+                                <i class="fas fa-eye"></i>
+                            </a>
+
+                            <a href="#" class="btn btn-sm text-decoration-none">
+                                <i class="fas fa-edit"></i>
+                            </a>
+
+                            <a href="#" class="btn btn-sm text-decoration-none">
+                                <i class="fas fa-trash text-danger"></i>
+                            </a>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+
+    @if (isset($rooms) and $rooms->count())
+        {{ $rooms->links() }}
+    @else
+        <p class="text-center py-4">No record found.</p>
+    @endif
 </div>
