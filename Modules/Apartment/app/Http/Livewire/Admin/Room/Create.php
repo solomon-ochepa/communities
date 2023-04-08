4b0f2f8c5129725f90c6a126a7b6db85e55ca818
Solomon Ochepa <110875083+solomon-ochepa@users.noreply.github.com>
2023-04-08 14:26:05 +0100

    Update Create.php

--- Modules/Apartment/app/Http/Livewire/Admin/Room/Create.php
+++ Modules/Apartment/app/Http/Livewire/Admin/Room/Create.php
@@ -3,11 +3,58 @@
 namespace Modules\Apartment\app\Http\Livewire\Admin\Room;
 
 use Livewire\Component;
+use Modules\Apartment\app\Http\Requests\StoreApartmentRoomRequest;
+use Modules\Room\app\Models\Room;
 
 class Create extends Component
 {
+    /** @var Apartment $apartment room's apartment */
+    public $apartment;
+
+    /** @var Room $room room to be created */
+    public $room;
+
+    protected $listeners = ['refresh' => '$refresh'];
+
+    /**
+     * Validation rules
+     **/
+    protected function rules()
+    {
+        $request = new StoreApartmentRoomRequest();
+        return $request->rules();
+    }
+
+    public function mount()
+    {
+        $this->room = new Room();
+    }
+
     public function render()
     {
         return view('apartment::livewire.admin.room.create');
+    }
+
+    public function store()
+    {
+        $this->validate();
+
+        if (!$this->apartment) {
+            session()->flash('error', "Apartment not valid.");
+            return;
+        }
+
+        // Apartment has room
+        if ($this->apartment->rooms()->where(['name' => $this->room->name])->count()) {
+            session()->flash('status', "Room already exists.");
+            return;
+        }
+
+        // Store
+        $this->apartment->rooms()->save($this->room);
+
+        session()->flash('status', "Room ({$this->room->name}) created successfully.");
+        $this->reset();
+        $this->emitUp('refresh');
     }
 }
