41b1bbdad20ed0f57c6e0161656e17371b4bdf2e
Solomon Ochepa <110875083+solomon-ochepa@users.noreply.github.com>
2023-04-08 09:40:22 +0100

    Updates

---
+++ Modules/Apartment/app/Http/Livewire/Admin/Room/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Modules\Apartment\app\Http\Livewire\Admin\Room;
+
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+        return view('apartment::livewire.admin.room.index');
+    }
+}
--- Modules/Apartment/resources/views/admin/room/index.blade.php
+++ Modules/Apartment/resources/views/admin/room/index.blade.php
@@ -1,9 +1,9 @@
 <x-app-layout :data="$head ?? []">
     <x-slot name="header">
         <h2 class="h3 m-0">
-            <x-back :url="route('office.apartment.show', $apartment->slug)" />
+            <x-back :url="route('admin.apartment.show', $apartment->slug)" />
 
-            {{ __('room.rooms') }} &middot; {{ $apartment->name }}
+            {{ __('Apartment Rooms Management') }} &middot; {{ $apartment->name }}
         </h2>
     </x-slot>
 
@@ -12,15 +12,15 @@
             @can('room.create')
                 <div class="card-header">
                     <!-- Create modal -->
-                    <a href="{{ route('office.apartment.room.create', ['apartment' => $apartment->slug]) }}"
+                    <a href="{{ route('admin.apartment.room.create', ['apartment' => $apartment->slug]) }}"
                         class="btn btn-icon icon-left btn-primary">
                         <i class="fas fa-plus"></i>
-                        {{ __('label.create') }}
+                        {{ __('Create') }}
                     </a>
                 </div>
             @endcan
 
-            <livewire:office.apartment.room.index :apartment="$apartment" />
+            <livewire:apartment::admin.room.index :apartment="$apartment" />
         </div>
     </div>
 </x-app-layout>
