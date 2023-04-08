5d53a436083f68f500537fb1d7a3460f8c34dbdd
Solomon Ochepa <110875083+solomon-ochepa@users.noreply.github.com>
2023-04-08 14:26:14 +0100

    Create StoreApartmentRoomRequest.php

---
+++ Modules/Apartment/app/Http/Requests/StoreApartmentRoomRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Modules\Apartment\app\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreApartmentRoomRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'room.name' => ['required', 'string', 'max:32'],
+            'room.active' => ['nullable', 'boolean'],
+        ];
+    }
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+        // return $this->user()->can('example.create');
+    }
+}
