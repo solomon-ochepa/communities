364917696640e82db7dcbab47d83989f89533120
Solomon Ochepa <110875083+solomon-ochepa@users.noreply.github.com>
2023-04-12 18:08:55 +0100

    Delete Noticeboard
    Create Notice

--- Modules/Noticeboard/resources/views/livewire/admin/edit.blade.php
+++
@@ -1,3 +0,0 @@
-<div>
-    <h3>The <code>Edit</code> livewire component is loaded from the <code>Noticeboard</code> module.</h3>
-</div>
--- Modules/Noticeboard/resources/views/livewire/admin/show.blade.php
+++
@@ -1,3 +0,0 @@
-<div>
-    <h3>The <code>Show</code> livewire component is loaded from the <code>Noticeboard</code> module.</h3>
-</div>
