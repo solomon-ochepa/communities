140dc9d4b673448d8d66fde9b7a0d725a0f87bfe
Solomon Ochepa <110875083+solomon-ochepa@users.noreply.github.com>
2023-04-19 17:11:12 +0100

    Update user-profile.blade.php
    Implement Logout

--- resources/views/components/layouts/app/navbar/user-profile.blade.php
+++ resources/views/components/layouts/app/navbar/user-profile.blade.php
@@ -15,7 +15,7 @@
     </div>
 
     <div class="dropdown-item">
-        <a href="{{ route('profile.edit') }}">
+        <a href="{{ route('profile.edit') }}" class="d-flex">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
                 class="feather feather-user">
@@ -52,15 +52,22 @@
     </div> --}}
 
     <div class="dropdown-item">
-        <a href="#">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-                class="feather feather-log-out">
-                <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
-                <polyline points="16 17 21 12 16 7"></polyline>
-                <line x1="21" y1="12" x2="9" y2="12"></line>
-            </svg>
-            <span>Log Out</span>
-        </a>
+        <!-- Authentication -->
+        <form method="POST" action="{{ route('logout') }}">
+            @csrf
+
+            <x-responsive-nav-link :href="route('logout')" class="d-flex"
+                onclick="event.preventDefault();
+                this.closest('form').submit();">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                    class="feather feather-log-out">
+                    <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
+                    <polyline points="16 17 21 12 16 7"></polyline>
+                    <line x1="21" y1="12" x2="9" y2="12"></line>
+                </svg>
+                <span>{{ __('Log Out') }}</span>
+            </x-responsive-nav-link>
+        </form>
     </div>
 </div>
