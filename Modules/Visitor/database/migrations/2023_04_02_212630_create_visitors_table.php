d4a56c01be4020b5d7b3d002e0d8b83154cc831b
Solomon Ochepa <110875083+solomon-ochepa@users.noreply.github.com>
2023-04-30 11:51:13 +0100

    Update 2023_04_02_212630_create_visitors_table.php
    + add active field

--- Modules/Visitor/database/migrations/2023_04_02_212630_create_visitors_table.php
+++ Modules/Visitor/database/migrations/2023_04_02_212630_create_visitors_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('visitors', function (Blueprint $table) {
             $table->uuid('id')->primary();
+            $table->boolean('active')->default(1);
             $table->foreignUuid('user_id')->unique();
             $table->timestamps();
             $table->softDeletes();
