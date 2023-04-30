ba6813a650c138167fe3a080ed60475dc20a1989
Solomon Ochepa <110875083+solomon-ochepa@users.noreply.github.com>
2023-04-30 05:55:48 +0100

    Update web.php

--- Modules/Like/routes/web.php
+++ Modules/Like/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Modules\Like\app\Http\Controllers\LikeController;
 
 /*
     |--------------------------------------------------------------------------
