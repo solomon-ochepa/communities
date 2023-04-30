abbdefdbdedf992e897599fb9af7d86e1b1ae678
Solomon Ochepa <110875083+solomon-ochepa@users.noreply.github.com>
2023-04-30 05:54:15 +0100

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,39 +2,8 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-
-// use Illuminate\Database\Eloquent\Casts\Attribute;
-// use Illuminate\Database\Eloquent\Concerns\HasUuids;
-// use Illuminate\Database\Eloquent\Factories\HasFactory;
-// use Illuminate\Database\Eloquent\SoftDeletes;
-// use Illuminate\Foundation\Auth\User as Authenticatable;
-// use Illuminate\Notifications\Notifiable;
-// use Laravel\Sanctum\HasApiTokens;
 use Modules\User\app\Models\User as ModulesUser;
-// use Plank\Mediable\Mediable;
-// use Spatie\Permission\Traits\HasRoles;
 
 class User extends ModulesUser
 {
-    // use HasApiTokens, HasFactory, Notifiable, Mediable, HasRoles, SoftDeletes, HasUuids;
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
 }
