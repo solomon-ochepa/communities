505e46923bb983075f87edc3d885339360caaa51
Solomon Ochepa <110875083+solomon-ochepa@users.noreply.github.com>
2023-04-30 05:54:28 +0100

    Update web.php

--- Modules/Comment/routes/web.php
+++ Modules/Comment/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Modules\Comment\app\Http\Controllers\CommentController;
 
 /*
     |--------------------------------------------------------------------------
