581108fb722c65a1ff3aa7680204f9c2bb452ed3
Solomon Ochepa <110875083+solomon-ochepa@users.noreply.github.com>
2023-04-30 05:56:46 +0100

    Update web.php

--- Modules/Setting/routes/web.php
+++ Modules/Setting/routes/web.php
@@ -1,16 +1,21 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+use Modules\Setting\app\Http\Controllers\SettingController;
+
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
+    |--------------------------------------------------------------------------
+    | Web Routes
+    |--------------------------------------------------------------------------
+    |
+    | Here is where you can register web routes for your application. These
+    | routes are loaded by the RouteServiceProvider within a group which
+    | contains the "web" middleware group. Now create something great!
+    |
 */
 
-Route::prefix('setting')->group(function() {
-    Route::get('/', 'SettingController@index');
+Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.')->group(function () {
+    Route::resource('setting', SettingController::class)->except(['index'])->names('setting');
+    Route::get('settings', [SettingController::class, 'index'])->name('setting.index');
+    Route::get('setting', fn () => redirect(route('admin.setting.index'), 302))->name('setting.fallback')->fallback();
 });
