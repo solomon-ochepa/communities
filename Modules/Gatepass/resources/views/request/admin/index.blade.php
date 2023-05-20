6c01e781192b10fb28ac1d9003c125f713dcc6fb
Solomon Ochepa <110875083+solomon-ochepa@users.noreply.github.com>
2023-05-20 07:49:37 +0100

    Update index.blade.php

--- Modules/Gatepass/resources/views/request/admin/index.blade.php
+++ Modules/Gatepass/resources/views/request/admin/index.blade.php
@@ -77,7 +77,7 @@
                 </div>
 
                 <div class="searchable-items list">
-                    {{-- Items Header --}}
+                    {{-- List Head --}}
                     <div class="items items-header-section">
                         <div class="item-content">
                             <div class="d-inline-flex">
@@ -91,15 +91,19 @@
 
                                 <h4>Name</h4>
                             </div>
+
                             <div class="user-email">
-                                <h4>Expectd Time Of Arrival</h4>
+                                <h4>Arrival</h4>
                             </div>
+
                             <div class="user-location">
                                 <h4 style="margin-left: 0;">Location</h4>
                             </div>
+
                             <div class="user-phone">
                                 <h4 style="margin-left: 3px;">Phone</h4>
                             </div>
+
                             <div class="action-btn">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                                     viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
@@ -116,8 +120,12 @@
                         </div>
                     </div>
 
+                    {{-- List Body --}}
                     @forelse ($gatepass->requests ?? [] as $request)
+                        {{-- @dd($request) --}}
                         {{-- @dd($request->requestable) --}}
+                        {{-- @dd($request->requestable->visitable) --}}
+                        {{-- @dd($request->requestable->visitable->user) --}}
                         <div class="items">
                             <div class="item-content">
                                 <div class="user-profile">
@@ -129,7 +137,9 @@
                                     </div>
                                     <img src="{{ asset('assets/app') }}/src/assets/img/profile-5.jpg" alt="avatar">
                                     <div class="user-meta-info">
-                                        <p class="user-name" data-name="Alan Green">Alan Green</p>
+                                        <p class="user-name"
+                                            data-name="{{ $request->requestable->visitable->user->name }}">
+                                            {{ $request->requestable->visitable->user->name }}</p>
                                         <p class="user-work" data-occupation="Web Developer">Web
                                             Developer</p>
                                     </div>
@@ -177,58 +187,6 @@
                             No request found.
                         </div>
                     @endforelse
-                    {{--
-                    <div class="items">
-                        <div class="item-content">
-                            <div class="user-profile">
-                                <div class="n-chk align-self-center text-center">
-                                    <div class="form-check form-check-primary me-0 mb-0">
-                                        <input class="form-check-input inbox-chkbox contact-chkbox" type="checkbox">
-                                    </div>
-                                </div>
-                                <img src="{{ asset('assets/app') }}/src/assets/img/profile-11.jpg" alt="avatar">
-                                <div class="user-meta-info">
-                                    <p class="user-name" data-name="Linda Nelson">Linda Nelson</p>
-                                    <p class="user-work" data-occupation="Web Designer">Web Designer
-                                    </p>
-                                </div>
-                            </div>
-                            <div class="user-email">
-                                <p class="info-title">Email: </p>
-                                <p class="usr-email-addr" data-email="linda@mail.com">linda@mail.com
-                                </p>
-                            </div>
-                            <div class="user-location">
-                                <p class="info-title">Location: </p>
-                                <p class="usr-location" data-location="Sydney, Australia">Sydney,
-                                    Australia</p>
-                            </div>
-                            <div class="user-phone">
-                                <p class="info-title">Phone: </p>
-                                <p class="usr-ph-no" data-phone="+1 (070) 123-4567">+1 (070)
-                                    123-4567</p>
-                            </div>
-                            <div class="action-btn">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                    viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                    stroke-linecap="round" stroke-linejoin="round"
-                                    class="feather feather-edit-2 edit">
-                                    <path d="M17 3a2.828 2.828 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5L17 3z">
-                                    </path>
-                                </svg>
-
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                    viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                                    stroke-linecap="round" stroke-linejoin="round"
-                                    class="feather feather-user-minus delete">
-                                    <path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path>
-                                    <circle cx="8.5" cy="7" r="4"></circle>
-                                    <line x1="23" y1="11" x2="17" y2="11"></line>
-                                </svg>
-                            </div>
-                        </div>
-                    </div>
-                     --}}
                 </div>
             </div>
         </div>
