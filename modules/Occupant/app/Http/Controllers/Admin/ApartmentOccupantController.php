d948e99d5bd3f06392be808ca5a0b6972b3144d0
Solomon Ochepa <solomonochepa@gmail.com>
2024-08-21 07:53:02 +0100

    Create Admin OccupantController and ApartmentOccupantController

---
+++ modules/Occupant/app/Http/Controllers/Admin/ApartmentOccupantController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Modules\Occupant\App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Modules\Apartment\app\Models\Apartment;
+use Modules\Occupant\App\Models\Occupant;
+
+class ApartmentOccupantController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware(['permission:apartment.occupant.index'])->only('index');
+        $this->middleware(['permission:apartment.occupant.create'])->only('create', 'store');
+        $this->middleware(['permission:apartment.occupant.edit'])->only('edit', 'update');
+        $this->middleware(['permission:apartment.occupant.delete'])->only('destroy');
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Apartment $apartment)
+    {
+        $data['head']['title'] = __('Occupants') . ' - ' . $apartment->name;
+        $data['apartment'] = $apartment;
+
+        return view('apartment::admin.occupant.index', $data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create(Apartment $apartment)
+    {
+        $data['head']['title'] = __('Create Occupant') . ' - ' . $apartment->name;
+        $data['apartment'] = $apartment;
+
+        return view('apartment::admin.occupant.create', $data);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request, Apartment $apartment)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Apartment $apartment, Occupant $occupant)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Apartment $apartment, Occupant $occupant)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Apartment $apartment, Occupant $occupant)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Apartment $apartment, Occupant $occupant)
+    {
+        //
+    }
+}
---
+++ modules/Occupant/app/Http/Controllers/Admin/OccupantController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Modules\Occupant\App\Http\Controllers\Admin;
+
+use Illuminate\Contracts\Support\Renderable;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Modules\Occupant\App\Models\Occupant;
+
+class OccupantController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware(['permission:apartment.occupant.index'])->only('index');
+        $this->middleware(['permission:apartment.occupant.create'])->only('create', 'store');
+        $this->middleware(['permission:apartment.occupant.edit'])->only('edit', 'update');
+        $this->middleware(['permission:apartment.occupant.delete'])->only('destroy');
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $data['head']['title']    = __('Tenants Management');
+
+        return view('occupant::admin.index', $data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        $data['head']['title']    = __('Create Occupant');
+
+        return view('occupant::admin.create', $data);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Show the specified resource.
+     */
+    public function show(Occupant $occupant)
+    {
+        return view('occupant::admin.show');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     * @param Occupant $occupant
+     * @return Renderable
+     */
+    public function edit(Occupant $occupant)
+    {
+        return view('occupant::admin.edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     * @param Request $request
+     * @param Occupant $occupant
+     * @return Renderable
+     */
+    public function update(Request $request, Occupant $occupant)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     * @param Occupant $occupant
+     * @return Renderable
+     */
+    public function destroy(Occupant $occupant)
+    {
+        // Room: change status to pending if occupant is the only occupant.
+        if ($occupant->room and !$occupant->room->occupants->except([$occupant->id])->count()) {
+            $occupant->room->update(['status_code' => 1]);
+        }
+
+        // Apartment: change status to pending if occupant is the only occupant.
+        if (!$occupant->apartment->occupants->except([$occupant->id])->count()) {
+            $occupant->apartment->update(['status_code' => 1]);
+        }
+
+        $occupant->delete();
+
+        session()->flash('status', 'Occupant deleted successfully.');
+        return back();
+    }
+}
