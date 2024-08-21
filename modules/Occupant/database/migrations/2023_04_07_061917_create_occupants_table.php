9af8f1f92f6cb4b0558a9dab2636a8b61077e449
Solomon Ochepa <solomonochepa@gmail.com>
2024-08-21 07:56:22 +0100

    Create migration

---
+++ modules/Occupant/database/migrations/2023_04_07_061917_create_occupants_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('occupants', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->boolean('active')->default(1);
+            $table->foreignUuid('user_id');
+            $table->foreignUuid('apartment_id');
+            $table->foreignUuid('room_id')->nullable();
+            $table->foreignUuid('status_code')->default(1)->nullable();
+            $table->timestamp('moved_in')->nullable()->useCurrent();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->unique(['user_id', 'room_id'], 'occupant');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('occupants');
+    }
+};
